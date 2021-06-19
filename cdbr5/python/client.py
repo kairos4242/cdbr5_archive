b6bb6097d02ea11f40bf7340b86b664ae84f04e6
Riley Ward <riley4242@hotmail.com>
2021-06-19 14:54:33 -0600

    Encapsulate sending and receiving into a function and create function to test loopback interface response time

--- cdbr5/python/client.py
+++ cdbr5/python/client.py
@@ -1,6 +1,8 @@
 import socket
 import struct
 import time
+import timeit
+import random
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 8008
@@ -11,13 +13,19 @@ buffer_u8 = "b"
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 time.sleep(.1)
-message = 50
-data = 128
-buffer_type = buffer_u8+buffer_u16
-buffer = struct.pack('<'+buffer_type,*[message,data])
 
-s.send(buffer)
-time.sleep(.1)
-data = s.recv(1024)
-print('Received', repr(data)[50:])
+def send_and_receive():
+    message = 50
+    data = random.randint(1, 128)
+    buffer_type = buffer_u8+buffer_u16
+    buffer = struct.pack('<'+buffer_type,*[message,data])
+
+    s.send(buffer)
+    #time.sleep(.1)
+    data = s.recv(1024)
+    #print('Received', repr(data)[50:])
+
+def test_timing():
+    time_val = timeit.timeit("send_and_receive()", "from __main__ import send_and_receive; import random", number=1000)
+    print("Time: " + str(time_val) + " seconds")
 s.close()
