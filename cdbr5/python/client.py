99f41cc04cbae3ef63b00dfe58c4cd10ca649cb9
Riley Ward <riley4242@hotmail.com>
2021-06-19 14:24:31 -0600

    Create basic python client to send and receive messages from GML server

---
+++ cdbr5/python/client.py
@@ -0,0 +1,23 @@
+import socket
+import struct
+import time
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 8008
+
+buffer_u16 = "H"
+buffer_u8 = "b"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+time.sleep(.1)
+message = 50
+data = 128
+buffer_type = buffer_u8+buffer_u16
+buffer = struct.pack('<'+buffer_type,*[message,data])
+
+s.send(buffer)
+time.sleep(.1)
+data = s.recv(1024)
+print('Received', repr(data)[50:])
+s.close()
